bac79a71b4a4dc162b901fa7067a47ee6a459a94
nicholasalexanderholmgren <nicholas.holmgren@students.mccc.edu>
2018-04-23 03:08:02 -0400

    Abstract Word, Further Work
    Just added getters for the fields and had them be set at construction time.

--- machineModel/AbstractWord.java
+++ machineModel/AbstractWord.java
@@ -4,6 +4,20 @@ public abstract class AbstractWord {
 	private boolean immediate;
 	private boolean isPrimitive;
 	private int numberOfArguments;
+	public AbstractWord(int numberOfArgument, boolean isPrimitive, boolean isImmediate) {
+		numberOfArguments = numberOfArgument;
+		this.isPrimitive = isPrimitive;
+		immediate = isImmediate;
+	}
+	public boolean isImmediate() {
+		return immediate;
+	}
+	public int getNumberOfArguments() {
+		return numberOfArguments;
+	}
+	public boolean isPrimitive() {
+		return isPrimitive;
+	}
 	public abstract String toString();
 	public abstract byte[] evaluate(byte[] args);
 }
