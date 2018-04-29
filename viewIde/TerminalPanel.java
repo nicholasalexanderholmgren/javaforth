fc005e4b8f2b7efa1e8fe7cc89339cbd1f33c9e9
nicholasalexanderholmgren <nicholas.holmgren@students.mccc.edu>
2018-04-28 23:05:20 -0400

    Terminal Improved
    Now clears the input text field and moves the cursor to the start of the field when enter is pressed and subsequently released.

--- viewIde/TerminalPanel.java
+++ viewIde/TerminalPanel.java
@@ -1,21 +1,20 @@
 package edu.mccc.cos210.ds.fp.javaforth.viewIde;
 
-import edu.mccc.cos210.ds.fp.javaforth.machineModel.*;
-
 import java.awt.Font;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import javax.swing.JComponent;
 //import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EtchedBorder;
+import edu.mccc.cos210.ds.fp.javaforth.machineModel.ForthMachine;
 
 public class TerminalPanel extends JScrollPane {
+	//JTextArea inputTextArea;
+	//JComponent outputTextArea;
 	JTextArea textArea;
 	String input;
+	JComponent contents;
 	public TerminalPanel(ForthMachine machine , StackPanel sp) {
 		super(new JTextArea(), JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		this.textArea = (JTextArea) this.getViewport().getView();
@@ -23,13 +22,17 @@ public class TerminalPanel extends JScrollPane {
 		this.textArea.setFont(new Font(Font.SERIF, Font.ITALIC, 28));
 		this.textArea.addKeyListener(new KeyAdapter() {
 			@Override
-			public void keyPressed(KeyEvent ke) {
+			public void keyReleased(KeyEvent ke) {
 				if (ke.getKeyCode() == KeyEvent.VK_ENTER) {
 					//Need to find a way to do this line by line
 					input = textArea.getText();
-					machine.interpret(input);
+					if(input.length()>0) {
+						machine.interpret(input);
+					}
+					textArea.setText("");
 					System.out.println("Your string is " + input);
 					sp.paintAll(getGraphics());
+					textArea.moveCaretPosition(0);
 				}
 			}
 		});
