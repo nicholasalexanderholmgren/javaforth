8ed5b90b2f26422c50d881a97a78f124ee9a8a94
nicholasalexanderholmgren <nicholas.holmgren@students.mccc.edu>
2018-04-30 07:45:21 -0400

    Interpret revision
    Major changes to how interpret works. Interpreter now uses the new Instrunction enum and the new system for variable and word lookup.
    
    Implemented ADD, JMP, and CJMP.

--- machineModel/ForthInterpretor.java
+++ machineModel/ForthInterpretor.java
@@ -1,18 +1,15 @@
 package edu.mccc.cos210.ds.fp.javaforth.machineModel;
 
-import java.util.List;
-import java.util.StringTokenizer;
-
 public class ForthInterpretor implements Runnable{
-	private int stringToInt;
 	private int debugStackheight;
 	private String debugWord;
 	ForthMachine machine;
-	private boolean comment;
-	private boolean debugCondition;
-	private boolean debugMode;
+//	private boolean comment;
+//	private boolean debugCondition;
+//	private boolean debugMode;
 	// The pointer for where the interpreter currently is getting instruction from in memory
-	int instPoint;
+	// is 0 when the interpreter is just moving data from the input stream to the data stack
+	int instPointer;
 	private InputStream inputStream;
 	boolean haltFlag;
 	public ForthInterpretor(ForthMachine parent) {
@@ -32,16 +29,6 @@ public class ForthInterpretor implements Runnable{
 	public void setDebugStackheight(int debugStackheight) {
 		this.debugStackheight = debugStackheight;
 	}
-	
-	public boolean isInteger( String input )  {  
-	   try {  
-		  stringToInt = Integer.parseInt( input );  
-	      return true;  
-	   }  
-	   catch( Exception e ) {  
-	      return false;  
-	   }  
-	}
 	/**
 	 * Main method of this class. Should be invoked when the the other parts of the machine are online.
 	 * Other machine parts include data stack, return stack, instruction set look up, name lookup table, 
@@ -54,10 +41,58 @@ public class ForthInterpretor implements Runnable{
 		}
 	}
 	private void interpret(String token) {
-		
+		if(machine.getDictionary().contains(token)) {
+			instPointer = machine.getDictionary().findAddr(token);
+			machine.getReturnStack().push((byte) 0);
+			while (instPointer != 0) {
+				Byte byteCode = machine.getFromAddr(instPointer);
+				switch(Forth79InstructionSet.convert(byteCode)) {
+					case ADD:
+						int n1 = bytesToInt(machine.getDataStack().pop(), machine.getDataStack().pop());
+						int n2 = bytesToInt(machine.getDataStack().pop(), machine.getDataStack().pop());
+						byte[] ans = intToBytes(n1+n2);
+						machine.getDataStack().push(ans[0]);
+						machine.getDataStack().push(ans[1]);
+						break;
+					case JMP:
+						instPointer = bytesToAddr(machine.getDataStack().pop(), machine.getDataStack().pop());
+						break;
+					case CJMP:
+						if((bytesToInt(machine.getDataStack().pop(),machine.getDataStack().pop()) != 0)){
+							instPointer = 
+									bytesToAddr(machine.getDataStack().pop(), machine.getDataStack().pop());
+						}
+						break;
+					default:
+						break;
+				}
+			}
+			return;
+		}
+		if(machine.getVariables().contains(token)) {
+			byte[] addr = intToBytes(machine.getVariables().findAddr(token));
+			machine.getDataStack().push(addr[0]);
+			machine.getDataStack().push(addr[1]);
+			return;
+		}
+		try {
+			int i = Integer.parseInt(token);
+			byte[] val = intToBytes(i);
+			machine.getDataStack().push(val[0]);
+			machine.getDataStack().push(val[1]);
+			return;
+		}catch(NumberFormatException e) {
+			
+		}
+		if(token.matches("'[\\x00-\\x7F]'")) {
+			machine.getDataStack().push((byte) token.charAt(1));
+			return;
+		}
+		stop();
 	}
 	public void stop() {
 		haltFlag = true;
+		inputStream.flush();
 	}
 	/**
 	 * Method for setting a flag in the interpreter as to whether conditional
@@ -66,11 +101,6 @@ public class ForthInterpretor implements Runnable{
 	 */
 	public void setDegbug(boolean debugState) {
 
-	}
-	private void pushStack(List<Byte> in) {
-		for(Byte i : in) {
-			machine.getStack().push(i);
-		}
 	}
 	/**
 	 * @param byte a is the leading half of the integer
@@ -86,6 +116,17 @@ public class ForthInterpretor implements Runnable{
 		int trailingDigits = unsignByte(b);
 
 		return leadingDigits + trailingDigits;
+	}
+	private byte[] intToBytes(Integer n) {
+		byte[] ans = new byte[2];
+		if(n > Math.pow(2, 16)) {
+			n = n%((int)Math.pow(2, 16));
+		}
+		Integer nA = n/256;
+		Integer nB = n%256;
+		ans[0] = nA.byteValue();
+		ans[1] = nB.byteValue();
+		return ans;
 	}
 	/**
 	 * @param byte addra is the leading half of the address
