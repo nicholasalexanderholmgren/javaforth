f26bf1f208bedfde852509ce49f74de4730ac4cf
nicholasalexanderholmgren <nicholas.holmgren@students.mccc.edu>
2018-05-08 16:38:31 -0400

    Implemented 0 comparisons

--- src/edu/mccc/cos210/ds/fp/javaforth/words/ZeroEquals.java
+++ src/edu/mccc/cos210/ds/fp/javaforth/words/ZeroEquals.java
@@ -13,9 +13,16 @@ public class ZeroEquals extends ForthWordBase {
 	@Override
 	public void execute(IStack<Object> stack, ForthDictionary dict) {
 		try {
-			
+			Double n1 = (Double) stack.pop();
+			if (n1 == 0) {
+				stack.push(1);
+			}else {
+				stack.push(0);
+			}
 		}catch(NoSuchElementException e) {
 			throw new RuntimeException("Stack underflow error on word 0=");
+		}catch(ClassCastException e) {
+			throw new RuntimeException("Typing error on word 0=");
 		}catch(Exception e) {
 			throw new RuntimeException("Unknown error on word 0=");
 		}
