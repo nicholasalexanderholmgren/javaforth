f029fc7baf38fd87bf1a28ec9419110ec4165543
nicholasalexanderholmgren <nicholas.holmgren@students.mccc.edu>
2018-05-08 16:19:58 -0400

    < and > implemtented

--- src/edu/mccc/cos210/ds/fp/javaforth/words/LessThan.java
+++ src/edu/mccc/cos210/ds/fp/javaforth/words/LessThan.java
@@ -1,5 +1,7 @@
 package edu.mccc.cos210.ds.fp.javaforth.words;
 
+import java.util.NoSuchElementException;
+
 import edu.mccc.cos210.ds.IStack;
 import edu.mccc.cos210.ds.fp.javaforth.machineModel.ForthDictionary;
 import edu.mccc.cos210.ds.fp.javaforth.machineModel.ForthWordBase;
@@ -10,5 +12,20 @@ public class LessThan extends ForthWordBase {
 	}
 	@Override
 	public void execute(IStack<Object> stack, ForthDictionary dict) {
+		try {
+			Double n1 = (Double) stack.pop();
+			Double n2 = (Double) stack.pop();
+			if (n1 < n2) {
+				stack.push(1);
+			}else {
+				stack.push(0);
+			}
+		}catch(NoSuchElementException e) {
+			throw new RuntimeException("Stack underflow error on word <");
+		}catch(ClassCastException e) {
+			throw new RuntimeException("Typing error on word <");
+		}catch(Exception e) {
+			throw new RuntimeException("Error on word <");
+		}
 	}
 }
