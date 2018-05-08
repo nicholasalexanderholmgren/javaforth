b9c133b45f6deebbdf9d937a3ba262bb50920a85
nicholasalexanderholmgren <nicholas.holmgren@students.mccc.edu>
2018-05-08 16:29:47 -0400

    Implemented logical OR

--- src/edu/mccc/cos210/ds/fp/javaforth/words/Or.java
+++ src/edu/mccc/cos210/ds/fp/javaforth/words/Or.java
@@ -1,5 +1,7 @@
 package edu.mccc.cos210.ds.fp.javaforth.words;
 
+import java.util.NoSuchElementException;
+
 import edu.mccc.cos210.ds.IStack;
 import edu.mccc.cos210.ds.fp.javaforth.machineModel.ForthDictionary;
 import edu.mccc.cos210.ds.fp.javaforth.machineModel.ForthWordBase;
@@ -10,5 +12,12 @@ public class Or extends ForthWordBase {
 	}
 	@Override
 	public void execute(IStack<Object> stack, ForthDictionary dict) {
+		try {
+			int n1 = ((Double) stack.pop()).intValue();
+			int n2 = ((Double) stack.pop()).intValue();
+			stack.push(n1 | n2);
+		}catch(NoSuchElementException e){
+			throw new RuntimeException("Stack underflow error on word OR.");
+		}
 	}
 }
