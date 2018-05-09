045a5861dcb1a47d0a0d7e882b0b1479ef3ce09f
Lloyd Feng <fjch1997@outlook.com>
2018-05-09 18:48:22 -0400

    ...

--- src/edu/mccc/cos210/ds/fp/javaforth/graphicswords/FillQuadCurve.java
+++ src/edu/mccc/cos210/ds/fp/javaforth/graphicswords/FillQuadCurve.java
@@ -8,26 +8,26 @@ import edu.mccc.cos210.ds.fp.javaforth.machineModel.GraphicsWordBase;
 import edu.mccc.cos210.ds.fp.javaforth.machineModel.ITerminalOutput;
 import edu.mccc.cos210.ds.fp.javaforth.viewIde.GraphicsFrame;
 
-public class DrawQuadCurve extends GraphicsWordBase {
+public class FillQuadCurve extends GraphicsWordBase {
 	private double x1;
 	private double y1;
 	private double ctrlx;
 	private double ctrly;
 	private double x2;
 	private double y2;
-	public DrawQuadCurve(GraphicsFrame frame) {
-		super("DRAWQUADCURVE", "x1 y1 ctrlx ctrly x2 y2 -- Draws a Quadratic Curve using current corlor.", frame);
+	public FillQuadCurve(GraphicsFrame frame) {
+		super("FILLQUADCURVE", "x1 y1 ctrlx ctrly x2 y2 -- Fills a Quadratic Curve using current corlor.", frame);
 		// TODO Auto-generated constructor stub
 	}
 	@Override
 	public void draw(Graphics2D g2d) {
 		QuadCurve2D q = new QuadCurve2D.Double();
 		q.setCurve(x1, y1, ctrlx, ctrly, x2, y2);
-		g2d.draw(q);
+		g2d.fill(q);
 	}
 	@Override
 	public void execute(IStack<Object> stack, ForthDictionary dictionary, ITerminalOutput terminalOutput) {
-		DrawQuadCurve word = new DrawQuadCurve(getFrame());
+		FillQuadCurve word = new FillQuadCurve(getFrame());
 		word.y2 = (double) stack.pop();
 		word.x2 = (double) stack.pop();
 		word.ctrly = (double) stack.pop();
